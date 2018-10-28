db5001d92ef4c0fe627ffb42b3bfcdcdfede34ee
Andrij David <andrijdavid@gmail.com>
2018-10-28 23:36:22 +0100

    correct typo

--- effnet.py
+++ effnet.py
@@ -28,11 +28,11 @@ class EffNet(nn.Module):
         layers = [
             nn.Conv2d(3, ch_in, kernel_size=(1,1), stride=(1,1), bias=False, padding=0, dilation=(1,1)) if ch_in ==32 else nn.Conv2d(ch_in, ch_in, kernel_size=(1,1),stride=(1,1), bias=False, padding=0, dilation=(1,1)) ,
             self.make_post(ch_in),
-            # DepthWiseConvultion2D
+            # DepthWiseConvolution2D
             nn.Conv2d(ch_in, 1 * ch_in, groups=ch_in, kernel_size=(1, 3),stride=(1,1), padding=(0,1), bias=False, dilation=(1,1)),
             self.make_post(ch_in),
             nn.MaxPool2d(kernel_size=(2,1), stride=(2,1)),
-            # DepthWiseConvultion2D
+            # DepthWiseConvolution2D
             nn.Conv2d(ch_in, 1 * ch_in, groups=ch_in, kernel_size=(3, 1), stride=(1,1), padding=(1,0), bias=False, dilation=(1,1)),
             self.make_post(ch_in),
             nn.Conv2d(ch_in, ch_out, kernel_size=(1, 2), stride=(1, 2), bias=False, padding=(0,0), dilation=(1,1)),
